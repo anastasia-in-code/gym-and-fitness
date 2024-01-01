36d7a030da9e06cc5ccb1ebfc8090e603e4eb143
Anastasiia <anastasiia.perih@gmail.com>
2023-12-31 20:56:36 -0500

    responsive Navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -11,6 +12,8 @@ type Props = {
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
+  const isLargeScreen = useMediaQuery("(min-width: 1060px)")
+  const [isSetSideMenuActive, setIsSideMenuActive] = useState(false)
 
   return (
     <nav>
@@ -18,7 +21,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <img alt="logo" src={Logo}></img>
-            <div className={`${flexBetween} w-full`}>
+            {isLargeScreen ? (<div className={`${flexBetween} w-full`}>
               <div className={`${flexBetween} text-small gap-8`}>
                 <Link
                   selectedPage={selectedPage}
@@ -46,7 +49,14 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 <p>Sign In</p>
                 <p>Get a Member</p>
               </div>
-            </div>
+            </div> 
+            ) : (
+              <button className="rounded-full bg-secondary-500 p-2"
+              onClick={() => setIsSideMenuActive(!isSetSideMenuActive)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
         </div>
       </div>
