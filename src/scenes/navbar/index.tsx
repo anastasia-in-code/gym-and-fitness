33c7be5bf77b692c12731fc57e435b92983d84dd
Anastasiia <anastasiia.perih@gmail.com>
2023-12-31 21:25:52 -0500

    side menu items

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -13,8 +13,8 @@ type Props = {
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
-  const isLargeScreen = useMediaQuery("(min-width: 1060px)")
-  const [isSideMenuActive, setIsSideMenuActive] = useState(false)
+  const isLargeScreen = useMediaQuery("(min-width: 1060px)");
+  const [isSideMenuActive, setIsSideMenuActive] = useState(false);
 
   return (
     <nav>
@@ -22,38 +22,42 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <img alt="logo" src={Logo}></img>
-            {isLargeScreen ? (<div className={`${flexBetween} w-full`}>
-              <div className={`${flexBetween} text-small gap-8`}>
-                <Link
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                  page="Home"
-                />
-                <Link
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                  page="Benefits"
-                />
-                <Link
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                  page="Our Classes"
-                />
-                <Link
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                  page="Contact Us"
-                />
-              </div>
+            {isLargeScreen ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} text-small gap-8`}>
+                  <Link
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                    page="Home"
+                  />
+                  <Link
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                    page="Benefits"
+                  />
+                  <Link
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                    page="Our Classes"
+                  />
+                  <Link
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                    page="Contact Us"
+                  />
+                </div>
 
-              <div className={`${flexBetween} gap-8`}>
-                <p>Sign In</p>
-                <ActionButton setSelectedPage={setSelectedPage}>Get a Member</ActionButton>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Get a Member
+                  </ActionButton>
+                </div>
               </div>
-            </div> 
             ) : (
-              <button className="rounded-full bg-secondary-500 p-2"
-              onClick={() => setIsSideMenuActive(!isSideMenuActive)}
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsSideMenuActive(!isSideMenuActive)}
               >
                 <Bars3Icon className="h-6 w-6 text-white" />
               </button>
@@ -63,11 +67,33 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
       </div>
 
       {!isLargeScreen && isSideMenuActive && (
-        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+        <div className="fixed bottom-0 right-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
           <div className="flex justify-end p-12">
-            <button  onClick={() => setIsSideMenuActive(!isSideMenuActive)}>
+            <button onClick={() => setIsSideMenuActive(!isSideMenuActive)}>
               <XMarkIcon className="h-5 w-6 text-gray-400" />
             </button>
+          </div>
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+              page="Home"
+            />
+            <Link
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+              page="Benefits"
+            />
+            <Link
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+              page="Our Classes"
+            />
+            <Link
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+              page="Contact Us"
+            />
           </div>
         </div>
       )}
