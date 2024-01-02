be34103abfb3c972ff51c371ff46a326b4465938
Anastasiia <anastasiia.perih@gmail.com>
2024-01-01 21:38:11 -0500

    HText - header component

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,24 +1,27 @@
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
-import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
+import Htext from "@/shared/Htext";
 
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void;
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Benefits = ({setSelectedPage}: Props) => {
+const Benefits = ({ setSelectedPage }: Props) => {
   return (
-    <section
-    id="benefits"
-    className="mx-auto min-h-full w-5/6 py-20">
-        <motion.div         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
->
-            
-        </motion.div>
-
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        <Htext>MORE THAN JUST A GYM</Htext>
+      </motion.div>
     </section>
-  )
-}
+  );
+};
 
-export default Benefits
+export default Benefits;
