4825d3c50cdfcef767e707076f0ff1eeb6b42ed4
Anastasiia <anastasiia.perih@gmail.com>
2024-01-01 20:21:44 -0500

    Navbar background

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -8,17 +8,21 @@ import ActionButton from "@/shared/ActionButton";
 
 type Props = {
   selectedPage: SelectedPage;
+  isTopOfPage: boolean;
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
   const isLargeScreen = useMediaQuery("(min-width: 1060px)");
   const [isSideMenuActive, setIsSideMenuActive] = useState(false);
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <img alt="logo" src={Logo}></img>
