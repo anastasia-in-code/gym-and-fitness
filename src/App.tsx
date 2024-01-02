1b3c3b722a3acd22bc7f1de2b79eaa2020adce44
Anastasiia <anastasiia.perih@gmail.com>
2024-01-01 21:34:34 -0500

    Benefits component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import Navbar from "@/scenes/navbar";
-import Home from "@/scenes/home";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
+import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
+
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home,
@@ -28,6 +30,7 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
