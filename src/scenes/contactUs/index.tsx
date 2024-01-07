7d769d1c1d7ddff1130aa1b97f284a5c88801498
Anastasiia <anastasiia.perih@gmail.com>
2024-01-07 18:34:27 -0500

    ContactUs component styling

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -17,7 +17,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
   } = useForm();
 
   const inputStyle =
-    "w-full mt-5 rounded-lg bg-primary-300 px-5 py-3 placeholder-white";
+    "w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white";
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     const isValid = await trigger();
@@ -92,7 +92,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                 })}
               />
               {errors.name && (
-                <p className="mt-1 text-primary-500 ">
+                <p className="mt-1 mb-5 text-primary-500 ">
                   {errors.name.type === "required" && "This field is required"}
                   {errors.name.type === "maxLength" &&
                     "This field is max 100 characters"}
@@ -136,6 +136,30 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                 Submit
               </button>
             </form>
+          </motion.div>
+          <motion.div 
+          className="relative mt-16 basis-2/5 md:mt-0"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{
+            once: true,
+            amount: 0.5,
+          }}
+          transition={{
+            delay: 0.2,
+            duration: 1,
+          }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}>
+            <div className="md:before:content-evolvetext w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1]">
+              <img 
+              className="w-full" 
+              alt='contact us page graphic'
+              src={ContactUsPageGraphic}/>
+            </div>
+
           </motion.div>
         </div>
       </motion.div>
